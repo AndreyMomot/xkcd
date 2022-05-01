4d46f2cfa9e97decdd76597b1fc9ffb900489c56
Andrii Momot <momot.andrii@pm.bet>
2022-05-01 16:58:13 +0200

    init commit

---
+++ xkcd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xkcd
+//
+//  Created by Andrii Momot on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
