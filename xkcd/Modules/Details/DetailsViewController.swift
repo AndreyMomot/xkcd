311b96f5d3385d72d9185808500ba81ca483cb6c
Andrii Momot <momot.andrii@pm.bet>
2022-05-02 10:29:05 +0200

    feat(share): add share options

--- xkcd/Modules/Details/DetailsViewController.swift
+++ xkcd/Modules/Details/DetailsViewController.swift
@@ -12,9 +12,10 @@ final class DetailsViewController: BaseViewController {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
     @IBOutlet private weak var imageView: LoadableImageView!
+    @IBOutlet private weak var shareButton: UIButton!
     
     var viewModel: DetailsViewModelProtocol?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +23,9 @@ final class DetailsViewController: BaseViewController {
     }
     
     private func configureView() {
+        shareButton.layer.cornerRadius = 8
+        shareButton.setTitle("SHARE", for: .normal)
+        
         guard let comics = viewModel?.comics else { return }
         
         titleLabel.text = comics.title
@@ -32,4 +36,17 @@ final class DetailsViewController: BaseViewController {
         }
     }
     
+    private func share(comics: Comics) {
+        guard let image = imageView.image else { return }
+        
+        let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityVC.popoverPresentationController?.sourceView = view
+
+        present(activityVC, animated: true, completion: nil)
+    }
+    
+    @IBAction private func shareButtonAction(_ sender: Any) {
+        guard let comics = viewModel?.comics else { return }
+        share(comics: comics)
+    }
 }
